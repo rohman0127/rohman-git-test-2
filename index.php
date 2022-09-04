c1b9193655edc6ce15b7bcbd9d777fefc3ea58a5
rohman0127 <rohmanoffice27>
2022-09-04 20:47:51 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <body>
     <h1>Hello Word!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi sequi ratione exercitationem explicabo earum dolorem amet est praesentium, eveniet ad rerum commodi facere doloribus eligendi aspernatur cupiditate quaerat quos assumenda.</p>
 </body>
 
 </html>
